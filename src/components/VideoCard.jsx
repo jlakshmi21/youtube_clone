3aee4125f8fc435e86dfb6bbd5798adfc07ff627
jlakshmi21 <mungarajayalakshmidevi@gmail.com>
2024-10-15 12:42:17 +0530

    updated

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,7 +19,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } })  => {
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
                 <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
                 {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-                </Typography>~
+                </Typography>
             </Link>
             <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
                 <Typography variant="subtitle2" color="gray">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'Javascript mastery', icon: <CodeIcon />, },
+  { name: 'Java', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
